6c5834ece145d3fe7a095cd4b251f6d6aa5f18c7
zionist <zionism@list.ru>
2013-05-12 17:51:15 +0400

    update page

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -6,7 +6,6 @@ import (
 	"github.com/zionist/charitablefond/app/constants"
 	"github.com/zionist/charitablefond/app/models"
 	"labix.org/v2/mgo/bson"
-	"time"
 )
 
 type Application struct {
@@ -15,14 +14,6 @@ type Application struct {
 
 //Any method on a controller that is exported and returns a revel.Result may be treated as an Action.
 func (c Application) Index() revel.Result {
-	collection := Session.DB(c.Base).C("test")
-	type Person struct {
-		Name  string
-		Phone string
-		Time  time.Time
-	}
-	collection.Insert(&Person{"Ale", "+55 53 8116 9639", time.Now()},
-		&Person{"Cla", "+55 53 8402 8510", time.Now()})
 	hello := "Hello world"
 	revel.INFO.Println("started")
 	return c.Render(hello)
@@ -30,6 +21,7 @@ func (c Application) Index() revel.Result {
 
 //GET page
 func (c Application) Page(url string) revel.Result {
+	revel.INFO.Println("Application.Page started")
 	err, found := c.CheckPageExists(url)
 	if err != nil {
 		c.RenderError(err)
@@ -42,7 +34,6 @@ func (c Application) Page(url string) revel.Result {
 	if err = collection.Find(bson.M{"url": url}).One(&result); err != nil {
 		c.RenderError(err)
 	}
-	fmt.Println(result.Content)
 	c.RenderArgs["page_header"] = result.Header
 	c.RenderArgs["page_content"] = result.Content
 	return c.RenderTemplate("Application/Page.html")
@@ -54,23 +45,19 @@ func (c Application) AdminListPages() revel.Result {
 	return nil
 }
 
-//Create plain page
-func (c Application) AdminCreatePage() revel.Result {
-	return c.RenderTemplate("Application/AdminCreate.html")
-}
-
-//Update plain page
-func (c Application) AdminUpdatePage() revel.Result {
-	return nil
-}
-
 //Delete plain page
 func (c Application) AdminDeletePage() revel.Result {
 	return nil
 }
 
-//POST pages
+//Create creation page
+func (c Application) AdminCreatePage() revel.Result {
+	return c.RenderTemplate("Application/AdminCreatePage.html")
+}
+
+//POST create plain pages 
 func (c Application) CreatePage(page_header, page_content, page_url string) revel.Result {
+	revel.INFO.Println("Application.CreatePage started")
 	c.Validation.MinSize(page_header, 1).Message("Требуется заголовок")
 	c.Validation.MinSize(page_url, 1).Message("Требуется ссылка на страницу")
 	c.Validation.MinSize(page_content, 1).Message("Требуется контент")
@@ -81,7 +68,6 @@ func (c Application) CreatePage(page_header, page_content, page_url string) reve
 		return c.Redirect(Application.AdminCreatePage)
 	}
 	//TODO: Add permission (sessison check)
-	//Check page exists
 	err, found := c.CheckPageExists(page_url)
 	if err != nil {
 		c.RenderError(err)
@@ -97,7 +83,7 @@ func (c Application) CreatePage(page_header, page_content, page_url string) reve
 	if err := c.SavePage(p); err != nil {
 		c.RenderError(err)
 	}
-	return c.RenderTemplate("Application/AdminCreated.html")
+	return c.RenderTemplate("Application/AdminPageCreated.html")
 }
 
 func (c Application) CheckPageExists(url string) (err error, found bool) {
@@ -105,7 +91,6 @@ func (c Application) CheckPageExists(url string) (err error, found bool) {
 	result := models.Page{}
 	empty := models.Page{}
 	err = collection.Find(bson.M{"url": url}).One(&result)
-	fmt.Println(result)
 	if result != empty {
 		found = true
 	} else {
@@ -114,10 +99,63 @@ func (c Application) CheckPageExists(url string) (err error, found bool) {
 	return
 }
 
+//Create update page
+func (c Application) AdminUpdatePage(url string) revel.Result {
+	revel.INFO.Println("Application.UpdatePage started")
+	err, found := c.CheckPageExists(url)
+	if err != nil {
+		c.RenderError(err)
+	}
+	if found == false {
+		return c.NotFound("Страница не найдена")
+	}
+	collection := Session.DB(c.Base).C(constants.PageCollectionName)
+	result := models.Page{}
+	if err = collection.Find(bson.M{"url": url}).One(&result); err != nil {
+		c.RenderError(err)
+	}
+	c.RenderArgs["page_header"] = result.Header
+	c.RenderArgs["page_content"] = result.Content
+	c.RenderArgs["page_url"] = result.Url
+	return c.RenderTemplate("Application/AdminUpdatePage.html")
+}
+
+//POST update plain pages 
+func (c Application) UpdatePage(page_header, page_content, page_url string) revel.Result {
+	revel.INFO.Println("Application.UpdatePage started")
+	c.Validation.MinSize(page_header, 1).Message("Требуется заголовок")
+	c.Validation.MinSize(page_url, 1).Message("Требуется ссылка на страницу")
+	c.Validation.MinSize(page_content, 1).Message("Требуется контент")
+	if c.Validation.HasErrors() {
+		revel.INFO.Printf("CreatePage validation errors %v", c.Validation.Errors)
+		c.Validation.Keep()
+		c.FlashParams()
+		return c.Redirect(Application.AdminCreatePage)
+	}
+	//TODO: Add permission (session check)
+	//Get page by url
+	//TODO: Remove security hole (user can delete all pages with same url using hidden value)
+	//Remove all pages with same url
+	if err := c.DelPages(page_url); err != nil {
+		c.RenderError(err)
+	}
+	//Save page
+	p := models.Page{Header: page_header, Url: page_url, Content: page_content}
+	if err := c.SavePage(p); err != nil {
+		c.RenderError(err)
+	}
+	return c.Redirect("/admin/update/%s", page_url)
+}
+
+func (c Application) DelPages(url string) (err error) {
+	collection := Session.DB(c.Base).C(constants.PageCollectionName)
+	err = collection.Remove(bson.M{"url": url})
+	revel.INFO.Printf("Pages with url %s removed", url)
+	return
+}
+
 func (c Application) SavePage(p models.Page) (err error) {
 	collection := Session.DB(c.Base).C(constants.PageCollectionName)
-	//collection := Session.DB("fond").C("pages")
-	fmt.Println("%+v", Session)
 	err = collection.Insert(&p)
 	revel.INFO.Printf("Page %s saved", p.Url)
 	return
